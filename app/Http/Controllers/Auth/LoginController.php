8db8fa6a574cff3d614a44829776aa0602234eaf
sylvaincodes <adandesylvain2010@gmail.com>
2023-02-17 07:57:43 +0100

    Set Routes Middleware

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
+use Auth;
 
 class LoginController extends Controller
 {
@@ -44,7 +45,10 @@ class LoginController extends Controller
         $this->validateLogin($request);
 
         if ($this->attemptLogin($request)) {
+
+
             $user = $this->guard()->user();
+
             $user->generateToken();
 
             return response()->json([
@@ -54,4 +58,21 @@ class LoginController extends Controller
 
         return $this->sendFailedLoginResponse($request);
     }
+
+
+    public function logout(Request $request)
+    {
+        $user = Auth::guard('api')->user();
+        if ($user) {
+            $user->api_token = null;
+            $user->save();
+        }
+        return response()->json(['data' => $user], 200);
+    }
+
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        return response()->json(['error' => 'Unauthenticated'], 401);
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -20,27 +20,34 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-//Get all users
-Route::get('users', 'Admin\UserController@index');
+
+Route::group(['middleware' => 'auth:api'], function () {
+
+    //Get all users
+    Route::get('users', 'Admin\UserController@index');
+
+    //Get one user
+    Route::get('users/{id}', 'Admin\UserController@show');
+
+    // Create a user
+    Route::post('users', 'Admin\UserController@store');
 
 
-//Get one user
-Route::get('users/{id}', 'Admin\UserController@show');
-
-// Create a user
-Route::post('users', 'Admin\UserController@store');
+    //Update a user
+    Route::get('users/{id}', 'Admin\UserController@upadte');
 
 
-//Update a user
-Route::get('users/{id}', 'Admin\UserController@upadte');
+    //Delete a user
+    Route::get('users/{id}', 'Admin\UserController@delete');
 
-
-//Delete a user
-Route::get('users/{id}', 'Admin\UserController@delete');
-
+});
 
 //Registered user endpoint
 Route::post('register', 'Auth\RegisterController@register');
 
+
 //Login user end point
 Route::post('login', 'Auth\LoginController@login');
+
+
+Route::post('logout', 'Auth\LoginController@logout');
